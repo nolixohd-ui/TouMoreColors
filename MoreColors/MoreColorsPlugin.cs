269a47c51c59b474229eec0421efe454d142d6a9
Nolixo <nolixohd@gmail.com>
2026-05-20 21:35:15 +0200

    Add files via upload

---
+++ MoreColors/MoreColorsPlugin.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using MiraAPI;
+using MiraAPI.PluginLoading;
+using Reactor;
+using Reactor.Networking;
+using Reactor.Networking.Attributes;
+using Reactor.Utilities;
+using TownOfUs;
+using MoreColors;
+
+namespace MoreColors;
+
+[BepInAutoPlugin("auavengers.tou.morecolors", "Tou: More Colors")]
+[BepInProcess("Among Us.exe")]
+[BepInDependency(ReactorPlugin.Id)]
+[BepInDependency(MiraApiPlugin.Id)]
+[BepInDependency(TownOfUsPlugin.Id)]
+[ReactorModFlags(ModFlags.RequireOnAllClients)]
+public partial class TouExtensionExamplePlugin : BasePlugin, IMiraPlugin
+{
+    /// <summary>
+    ///     Gets the specified Culture for string manipulations.
+    /// </summary>
+    public static CultureInfo Culture => TownOfUs.TownOfUsPlugin.Culture;
+
+    /// <inheritdoc />
+    public string OptionsTitleText => "More Colors";
+
+    /// <summary>
+    ///     Determines if the current build is a dev build or not. This will change certain visuals as well as always grab news locally to be up to date.
+    /// </summary>
+    public static bool IsDevBuild => true;
+
+    /// <inheritdoc />
+    public ConfigFile GetConfigFile()
+    {
+        return Config;
+    }
+
+    public Harmony Harmony { get; } = new(Id);
+
+    public override void Load()
+    {
+        ReactorCredits.Register("Tou: More Colors", Version, IsDevBuild, ReactorCredits.AlwaysShow);
+        IL2CPPChainloader.Instance.Finished += Modules.ColorsLocale.SearchInternalLocale; // Initialise AFTER the mods are loaded to ensure maximum parity (no need for the soft dependency either then)
+
+        Harmony.PatchAll();
+    }
+}
